8d6093a5e6fc2cd5aefd8b83e68c2839aaf63467
mynameisonoda <tyleronodera@gmail.com>
2022-11-13 22:23:08 -0800

    Refactor update attempt two

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Button(props) {
     return (
         <label className="text-2xl flex border rounded-md py-3 px-4 items-center justify-center text-sm font-medium hover:bg-gray-50 focus:outline-none sm:flex-1 bg-[#00494d] shadow-sm text-white cursor-pointer">
-            <input type="radio" name="size-choice" value="0.05" className="sr-only" />
-            <span>5%</span>
-            <span className="pointer-events-none absolute -inset-px rounded-md" aria-hidden="true"></span>
+            <input type="radio" name="size-choice" value={props.percent_value} className="sr-only" />
+            <span>{props.percent_text}</span>
+            <span className="pointer-events-none absolute rounded-md" aria-hidden="true"></span>
         </label>
     );
 }
