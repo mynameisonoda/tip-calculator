de56e32b233407e1d4922127fcf8951344814ba2
mynameisonoda <tyleronodera@gmail.com>
2023-01-08 22:18:52 -0800

    Made progress on handling changes to input fields, used switch case, gotta figure out how to set values depending on event name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 function App() {
+
+    const defaultValues = {
+        billAmount: "0",
+        selectedTip: "0",
+        numOfPeople: "0",
+        tipAmount: "0",
+        splitPayAmount: "0",
+    };
+    const [value, setValue] = useState(defaultValues);
+
+    function handleChange(event) {
+
+        let inputName = event.target.name;
+        let inputValue = event.target.value;
+
+        switch (inputName) {
+            case "bill":
+                //Note to self: figure out how to set values, everything else still works  :)
+                setValue(prevValue => {
+                    return {
+
+                        billAmount: inputValue,
+                        selectedTip: prevValue.selectedTip,
+                        numOfPeople: prevValue.numOfPeople,
+                        tipAmount: billAmount * selectedTip,
+                        splitPayAmount: (billAmount + tipAmount)/numOfPeople,
+                    }
+                })
+                console.log(value);
+                break;
+            case "tipPercent":
+                console.log("Tip input triggered");
+                break;
+            case "peopleCount":
+                console.log("People input triggered");
+                break;
+        }
+    };
+
     return (
         <div className="container relative border rounded-md mx-auto my-12 px-4 py-4 bg-slate-50 grid grid-cols-2 gap-8">
             <section name="calculations">
                 <form action="">
                     <section name="bill-section" className="grid grid-rows-2 py-8">
                         <label htmlFor="bill" className="py-2">Bill</label>
-                        <input type="text" name="bill" placeholder="0" className="rounded-md text-right pr-3"/>
+                        <input type="text" name="bill" placeholder="0" className="rounded-md text-right pr-3" onChange={handleChange} />
                     </section>
 
                     <section name="tip-selection">
@@ -16,27 +55,32 @@ function App() {
                         <div name="tip-selection-group" className="container grid grid-rows-2 grid-cols-3 gap-2 text-9xl">
 
                             <Button
-                            percent_text="5%"
-                            percent_value="0.05"
+                                percent_text="5%"
+                                percent_value="0.05"
+                                handleChange={handleChange}
                             />
                             <Button
-                            percent_text="10%"
-                            percent_value="0.1"
+                                percent_text="10%"
+                                percent_value="0.1"
+                                handleChange={handleChange}
                             />
                             <Button
-                            percent_text="15%"
-                            percent_value="0.15"
+                                percent_text="15%"
+                                percent_value="0.15"
+                                handleChange={handleChange}
                             />
                             <Button
-                            percent_text="25%"
-                            percent_value="0.25"
+                                percent_text="25%"
+                                percent_value="0.25"
+                                handleChange={handleChange}
                             />
                             <Button
-                            percent_text="50%"
-                            percent_value="0.5"
+                                percent_text="50%"
+                                percent_value="0.5"
+                                handleChange={handleChange}
                             />
                             <label className="flex border rounded-md py-3 px-4 items-center justify-center text-sm font-medium hover:bg-gray-50 focus:outline-none sm:flex-1 bg-[#00494d] shadow-sm text-white cursor-pointer">
-                                <input type="text" name="size-choice" className="sr-only" />
+                                <input type="text" name="tipPercent" className="sr-only" />
                                 <span>Custom</span>
                                 <span className="pointer-events-none absolute -inset-px rounded-md" aria-hidden="true"></span>
                             </label>
@@ -45,7 +89,7 @@ function App() {
                     </section>
                     <section className="grid grid-rows-2 py-8">
                         <label htmlFor="people-count" className="py-2">Number of People</label>
-                        <input type="text" name="people-count" placeholder="0" className="rounded-md text-right pr-3"/>
+                        <input type="text" name="peopleCount" placeholder="0" className="rounded-md text-right pr-3" onChange={handleChange} />
                     </section>
 
                 </form>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button(props) {
     return (
         <label className="text-2xl flex border rounded-md py-3 px-4 items-center justify-center text-sm font-medium hover:bg-gray-50 focus:outline-none sm:flex-1 bg-[#00494d] shadow-sm text-white cursor-pointer">
-            <input type="radio" name="size-choice" value={props.percent_value} className="sr-only" />
+            <input type="radio" name="tipPercent" value={props.percent_value} className="sr-only" onChange={props.handleChange}/>
             <span>{props.percent_text}</span>
             <span className="pointer-events-none absolute rounded-md" aria-hidden="true"></span>
         </label>
